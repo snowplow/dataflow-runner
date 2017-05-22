213b16ccc657542cb1344ba0f775ff6e654cc5a3
Ben Fradet <benjamin.fradet@gmail.com>
2017-05-22 16:41:17 +0100

    Add Consul-based locking (closes #17)

--- src/lock_test.go
+++ src/lock_test.go
@@ -19,9 +19,74 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/nightlyone/lockfile"
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/testutil"
 	"github.com/stretchr/testify/assert"
 )
+
+func makeClient(t *testing.T) (*api.Client, *testutil.TestServer) {
+	// Make client config
+	conf := api.DefaultConfig()
+	// Create server
+	server, err := testutil.NewTestServerConfigT(t, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conf.Address = server.HTTPAddr
+
+	// Create client
+	client, err := api.NewClient(conf)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	return client, server
+}
+
+func TestConsulLock(t *testing.T) {
+	assert := assert.New(t)
+
+	c, s := makeClient(t)
+	assert.NotNil(c)
+	assert.NotNil(s)
+	defer s.Stop()
+
+	lockName := "lock"
+
+	cl, err := InitConsulLock("some://faulty.address", lockName)
+	assert.NotNil(err)
+	assert.Nil(cl)
+	assert.Equal("Unknown protocol scheme: some", err.Error())
+
+	cl, err = InitConsulLock(s.HTTPAddr, lockName)
+	assert.Nil(err)
+	assert.NotNil(cl)
+
+	err = cl.TryLock()
+	assert.Nil(err)
+
+	// fail if already locked
+	err = cl.TryLock()
+	assert.NotNil(err)
+	assert.Equal(LockHeldError("lock already held at "+lockName), err)
+
+	// fail if already unlocked
+	err = cl.Unlock()
+	assert.Nil(err)
+
+	err = cl.Unlock()
+	assert.NotNil(err)
+	assert.Equal("lock not held", err.Error())
+
+	// fail for malformed key
+	cl, err = InitConsulLock(s.HTTPAddr, "/"+lockName)
+	assert.Nil(err)
+	assert.NotNil(cl)
+
+	err = cl.TryLock()
+	assert.NotNil(err)
+	assert.Equal("Invalid key. Key must not begin with a '/': /"+lockName, err.Error())
+}
 
 func TestFileLock(t *testing.T) {
 	assert := assert.New(t)
@@ -31,7 +96,7 @@ func TestFileLock(t *testing.T) {
 	fl, err := InitFileLock(lockPath)
 	assert.NotNil(fl)
 	assert.Nil(err)
-	assert.Equal(fl, &FileLock{lock: lockfile.Lockfile(lockPath)})
+	assert.Equal(fl, &FileLock{path: lockPath})
 
 	// write to the file so that we can't get a lock on it
 	pid := os.Getppid()
@@ -40,19 +105,54 @@ func TestFileLock(t *testing.T) {
 
 	err = fl.TryLock()
 	assert.NotNil(err)
-	assert.Equal(lockfile.ErrBusy, err)
-
-	// cleanup
-	err = os.Remove(lockPath)
-	assert.Nil(err)
-
-	err = fl.TryLock()
-	assert.Nil(err)
+	assert.Equal(LockHeldError("lock already held at "+lockPath), err)
 
 	err = fl.Unlock()
 	assert.Nil(err)
 
 	err = fl.Unlock()
 	assert.NotNil(err)
-	assert.Equal(lockfile.ErrRogueDeletion, err)
+	assert.Equal("remove "+lockPath+": no such file or directory", err.Error())
+
+	// fail for a non-existing path
+	otherLockPath := lockPath + "/something/else"
+	fl, err = InitFileLock(otherLockPath)
+	assert.NotNil(fl)
+	assert.Nil(err)
+	assert.Equal(fl, &FileLock{path: otherLockPath})
+
+	err = fl.TryLock()
+	assert.NotNil(err)
+	assert.Equal("open "+otherLockPath+": no such file or directory", err.Error())
+}
+
+func TestGetLock(t *testing.T) {
+	assert := assert.New(t)
+
+	c, s := makeClient(t)
+	assert.NotNil(c)
+	assert.NotNil(s)
+	defer s.Stop()
+
+	lockName := "/tmp/lock"
+
+	// FileLock if consul == ""
+	lock, err := GetLock(lockName, "")
+	assert.NotNil(lock)
+	assert.Nil(err)
+	assert.Equal(lock, &FileLock{path: lockName})
+
+	// ConsulLock if consul != ""
+	lock, err = GetLock(lockName, s.HTTPAddr)
+	assert.NotNil(lock)
+	assert.Nil(err)
+	cl, ok := lock.(*ConsulLock)
+	assert.NotNil(cl)
+	assert.Equal(true, ok)
+
+	// error otherwise
+	lock, err = GetLock(lockName, "some://faulty.address")
+	assert.Nil(lock)
+	assert.NotNil(err)
+	assert.Equal("Unknown protocol scheme: some", err.Error())
 }
