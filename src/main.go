1d5369bc037fe9229d2711c07d9738bbffa57c52
Ben Fradet <benjamin.fradet@gmail.com>
2018-01-25 12:45:25 +0000

    Prepared for release

--- src/main.go
+++ src/main.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2016-2017 Snowplow Analytics Ltd. All rights reserved.
+// Copyright (c) 2016-2018 Snowplow Analytics Ltd. All rights reserved.
 //
 // This program is licensed to you under the Apache License Version 2.0,
 // and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -30,8 +30,8 @@ import (
 const (
 	appName          = "dataflow-runner"
 	appUsage         = "Run templatable playbooks of Hadoop/Spark/et al jobs on Amazon EMR"
-	appCopyright     = "(c) 2016-2017 Snowplow Analytics Ltd"
-	cliVersion       = "0.3.0"
+	appCopyright     = "(c) 2016-2018 Snowplow Analytics Ltd"
+	cliVersion       = "0.4.0"
 	varDelim         = ","
 	fEmrConfig       = "emr-config"
 	fEmrPlaybook     = "emr-playbook"
