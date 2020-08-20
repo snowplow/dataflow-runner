02ca540985f98796467feb8fff20ef216ca18935
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2020-08-20 17:17:50 +0300

    Prepare for release

--- src/main.go
+++ src/main.go
@@ -32,7 +32,7 @@ const (
 	appName          = "dataflow-runner"
 	appUsage         = "Run templatable playbooks of Hadoop/Spark/et al jobs on Amazon EMR"
 	appCopyright     = "(c) 2016-2020 Snowplow Analytics Ltd"
-	cliVersion       = "0.4.2"
+	cliVersion       = "0.5.0"
 	varDelim         = ","
 	fEmrConfig       = "emr-config"
 	fEmrPlaybook     = "emr-playbook"
