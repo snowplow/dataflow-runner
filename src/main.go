96bde0d3d2c928fae77fcf98a0daf3d3d3834922
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2026-06-08 14:17:35 +0300

    Log EMR cluster StateChangeReason on transient mode failure (#100)

--- src/main.go
+++ src/main.go
@@ -327,6 +327,15 @@ func main() {
 					maxClusterWaitDuration,
 				)
 				if err != nil {
+					// The terminated waiter only returns a generic "transitioned to Failure" error, so
+					// re-describe the cluster to recover and surface the underlying StateChangeReason.
+					if code, message, derr := emrCluster.fetchStateChangeReason(context.Background(), jobFlowSteps.JobflowID); derr != nil {
+						log.Warnf("could not re-describe cluster to get StateChangeReason: %v", derr)
+					} else if code != "" || message != "" {
+						log.Errorf("EMR cluster state change reason: code='%s' message=%q", code, message)
+						err = fmt.Errorf("EMR cluster %s terminated with errors: code=%s message=%q",
+							jobFlowSteps.JobflowID, code, message)
+					}
 					if lock != nil && softLock != "" {
 						lock.Unlock()
 					}
